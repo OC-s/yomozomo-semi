3bc41e5046931da5241235e75945f5d885e2c886
경제목 <jemok9605@naver.com>
2022-11-22 17:29:27 +0900

    commit jm

---
+++ src/main/java/controller/EmailController.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login/search/email")
+public class EmailController extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		Properties p = new Properties();
+		String memerber = req.getParameter("email");
+		p.put("mail.smtp.starttls.enable", "true");
+        p.put("mail.smtp.host", "smtp.naver.com");
+        p.put("mail.smtp.auth", "true");
+        p.put("mail.smtp.port", "587");
+		//return new PasswordAuthentication("jemok9605@naver.com", ""); 
+		Session session = Session.getInstance(p, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				//보내는 관리자 email, password
+				return null;
+			}
+		});
+		
+		String receiver = memerber; // 메일 받을 주소
+		String title = "yomozomo 인증번호 입니다.";
+
+		String num ="12345678";
+		Message message = new MimeMessage(session);
+		try {
+//			보내는 사람 정보 
+			message.setFrom(new InternetAddress("jemok9605@naver.com", "관리자", "utf-8"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+			message.setSubject(title);
+			message.setContent(num,"text/html; charset=utf-8");
+			Transport.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+		 req.setAttribute("num", num);
+		 req.getRequestDispatcher("/login/search.jsp").forward(req, res);
+		 
+		}
+	}
+		
