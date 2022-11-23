18e3bda4492e82e4868d42fc09a2ee0ecd7bb987
KKW <plllvb1025@naver.com>
2022-11-23 12:27:43 +0900

    commit kkw 11/23 12:27

--- src/main/java/kr/co/yomozomo/dao/QacommentsDAO.java
+++ src/main/java/kr/co/yomozomo/dao/QacommentsDAO.java
@@ -162,6 +162,70 @@ public class QacommentsDAO {
 			e.printStackTrace();
 		}
 	}
+	
+	public int getTotal() {
+		sb.setLength(0);
+		sb.append("SELECT count(*) cnt FROM QACOMMENTS");
+		int count = 1;
+		try {
+			pstmt = conn.prepareStatement(sb.toString());
+			rs = pstmt.executeQuery();
+			rs.next();
+			count = rs.getInt("cnt");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;		
+	}
+	
+	// 전체조회(join)+페이징
+		public ArrayList<QacmtjoinVO> joinpageAll(int startNo, int endNo) {
+			ArrayList<QacmtjoinVO> list = new ArrayList<QacmtjoinVO>();
+			
+
+			sb.setLength(0);
+			sb.append("SELECT * FROM QACMT ");
+			sb.append("INNER JOIN MEMBER ON QACMT.M_NUM = MEMBER.M_NUM ");
+			sb.append("ORDER BY QAC_NUM DESC ");
+			sb.append("LIMIT ?, ? ");
+			
+			QacmtjoinVO vo = null;
+
+			try {
+				pstmt = conn.prepareStatement(sb.toString());
+				pstmt.setInt(1, startNo-1);
+				pstmt.setInt(2, endNo);
+				rs = pstmt.executeQuery();
+
+				while (rs.next()) {
+					int M_NUM = rs.getInt("M_NUM");
+					int QAB_NUM = rs.getInt("QAB_NUM");
+					String QAC_CONTENTS = rs.getString("QAC_CONTENTS");
+					int QAC_NUM = rs.getInt("QAC_NUM");
+					String QAC_REGDATE = rs.getString("QAC_REGDATE");
+					String QAC_SECRET = rs.getString("QAC_SECRET");
+
+					String ADDRESS = rs.getString("ADDRESS");
+					String ADDRESSDETAIL = rs.getString("ADDRESSDETAIL");
+					String EMAIL = rs.getString("EMAIL");
+					String ID = rs.getString("ID");
+					String NAME = rs.getString("NAME");
+					String NICKNAME = rs.getString("NICKNAME");
+					String PASSWORD = rs.getString("PASSWORD");
+					String PHONE = rs.getString("PHONE");
+					String REGDATE = rs.getString("REGDATE");
+					String ZIPCODE = rs.getString("ZIPCODE");
+					vo = new QacmtjoinVO(M_NUM, QAB_NUM, QAC_CONTENTS, QAC_NUM, QAC_REGDATE, QAC_SECRET, ADDRESS, ADDRESSDETAIL, EMAIL,
+							ID, NAME, NICKNAME, PASSWORD, PHONE, REGDATE, ZIPCODE);
+					list.add(vo);
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return list;
+		}
 
 	// 수정
 	public void updateOne(QacommentsVO vo) {
